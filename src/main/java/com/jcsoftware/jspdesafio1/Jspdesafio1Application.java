32c653802dac97993dea3131913ecd482842683e
Julio Cesar <jccintr@gmail.com>
2025-01-26 18:53:23 -0300

    Projeto criado

---
+++ src/main/java/com/jcsoftware/jspdesafio1/Jspdesafio1Application.java
@@ -0,0 +1,13 @@
+package com.jcsoftware.jspdesafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jspdesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jspdesafio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcsoftware/jspdesafio1/Jspdesafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcsoftware.jspdesafio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jspdesafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
