a053ed76eee127a09d2b33f569456ff909c6296a
Julio Cesar <jccintr@gmail.com>
2025-01-26 21:17:59 -0300

    Criado as classes do projeto

---
+++ src/main/java/com/jcsoftware/jspdesafio1/entities/Atividade.java
@@ -0,0 +1,75 @@
+package com.jcsoftware.jspdesafio1.entities;
+
+import java.util.Objects;
+
+public class Atividade {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private Double preco;
+	
+	public Atividade() {
+		
+	}
+
+	public Atividade(Integer id, String nome, String descricao, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, id, nome, preco);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Atividade other = (Atividade) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(preco, other.preco);
+	}
+	
+	
+}
---
+++ src/main/java/com/jcsoftware/jspdesafio1/entities/Categoria.java
@@ -0,0 +1,56 @@
+package com.jcsoftware.jspdesafio1.entities;
+
+import java.util.Objects;
+
+public class Categoria {
+
+	
+	private Integer id;
+	private String descricao;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
